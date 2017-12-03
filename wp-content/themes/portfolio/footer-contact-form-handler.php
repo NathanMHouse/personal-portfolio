43d0b78923cc626133f60326884e2f66896ca8a3
nathanmhouse <nathan.house@brainrider.com>
2017-12-03 06:54:42 -0500

    updated form handler js/ajax functionality

--- wp-content/themes/portfolio/footer-contact-form-handler.php
+++ wp-content/themes/portfolio/footer-contact-form-handler.php
@@ -27,7 +27,7 @@ $sender_email   = ( isset( $_POST['email'] ) ) // Input var okay.
 				  ? filter_var( wp_unslash( $_POST['email'] ), FILTER_SANITIZE_EMAIL ) // Input var okay.
 				  : '';
 $message        = ( isset( $_POST['message'] ) ) // Input var okay.
-				  ? filter_var( html_entity_decode( sanitize_text_field( $_POST['message'] ) ), FILTER_SANITIZE_STRING ) // Input var okay.
+				  ? filter_var( sanitize_text_field( wp_unslash( $_POST['message'] ) ), FILTER_SANITIZE_STRING ) // Input var okay.
 				  : '';
 
 //Create/run function to check for suspect patterns/unknown inputs
