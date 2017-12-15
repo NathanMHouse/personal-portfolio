2993899e01481f35ecc580781b3a04bc8c963dc3
nathanmhouse <nathan.house@brainrider.com>
2017-12-15 07:08:23 -0500

    updated secondary modal cta to include js scroll functionality

--- wp-content/themes/portfolio/template-parts/content-work.php
+++ wp-content/themes/portfolio/template-parts/content-work.php
@@ -150,6 +150,7 @@ $work_title = get_field( 'work_title', 'options' );
 									<a role="button"
 									   class="cta secondary-cta"
 									   href="<?php echo esc_attr( $modal_secondary_cta_url ); ?>"
+									   data-scroll
 								   	>
 										<?php echo esc_html( $modal_secondary_cta_text ); ?>
 									</a>
