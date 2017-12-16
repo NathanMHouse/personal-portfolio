e594ae96f4cc60351b87f09b63c3ed20b8f8ebd2
nathanmhouse <nathan.house@brainrider.com>
2017-12-16 10:45:18 -0500

    refactored portfolio plugin to pass phpcs using wordpress standards; updated min js;

--- wp-content/plugins/portfolio-plugin/portfolio-plugin.php
+++ wp-content/plugins/portfolio-plugin/portfolio-plugin.php
@@ -5,16 +5,17 @@ Plugin URI: nathanmhouse.com
 Description: The Portfolio Plugin adds functionality to the portfolio theme.
 Version: 0.1
 Author: Nathan M. House
-Author URI: 
+Author URI:
 Text Domain: portfolio-plugin
 Copyright: Nathan M. House
-License: 
+License:
 */
 
 /*--------------------------------------------------------------
 >>> TABLE OF CONTENTS:
 ----------------------------------------------------------------
 1.0 Custom Post-type Registration
+2.0 Custom Post-type Redirect
 
 */
 
@@ -22,131 +23,153 @@ License:
 /**
  * 1.0 Custom Post-type Registration
  *
- * 
+**/
+add_action( 'init', 'pp_register_post_types' );
+function pp_register_post_types() {
+
+	// Register Examples post type
+	register_post_type(
+		'pp_examples',
+		array(
+			'label'       => __( 'Examples', 'portfolio-plugin' ),
+			'description' => __( 'Portfolio work pieces.', 'portfolio-plugin' ),
+			'labels'      => array(
+				'name'                  => _x( 'Examples', 'Post Type General Name', 'portfolio-plugin' ),
+				'singular_name'         => _x( 'Example', 'Post Type Singular Name', 'portfolio-plugin' ),
+				'menu_name'             => __( 'Examples', 'portfolio-plugin' ),
+				'name_admin_bar'        => __( 'Example', 'portfolio-plugin' ),
+				'archives'              => __( 'Example Archives', 'portfolio-plugin' ),
+				'parent_item_colon'     => __( 'Parent Example:', 'portfolio-plugin' ),
+				'all_items'             => __( 'All Examples', 'portfolio-plugin' ),
+				'add_new_item'          => __( 'Add New Example', 'portfolio-plugin' ),
+				'add_new'               => __( 'Add New', 'portfolio-plugin' ),
+				'new_item'              => __( 'New Example', 'portfolio-plugin' ),
+				'edit_item'             => __( 'Edit Example', 'portfolio-plugin' ),
+				'update_item'           => __( 'Update Example', 'portfolio-plugin' ),
+				'view_item'             => __( 'View Example', 'portfolio-plugin' ),
+				'search_items'          => __( 'Search Example', 'portfolio-plugin' ),
+				'not_found'             => __( 'Not found', 'portfolio-plugin' ),
+				'not_found_in_trash'    => __( 'Not found in Trash', 'portfolio-plugin' ),
+				'featured_image'        => __( 'Featured Image', 'portfolio-plugin' ),
+				'set_featured_image'    => __( 'Set featured image', 'portfolio-plugin' ),
+				'remove_featured_image' => __( 'Remove featured image', 'portfolio-plugin' ),
+				'use_featured_image'    => __( 'Use as featured image', 'portfolio-plugin' ),
+				'insert_into_item'      => __( 'Insert into example', 'portfolio-plugin' ),
+				'uploaded_to_this_item' => __( 'Uploaded to this example', 'portfolio-plugin' ),
+				'items_list'            => __( 'Example list', 'portfolio-plugin' ),
+				'items_list_navigation' => __( 'Examples list navigation', 'portfolio-plugin' ),
+				'filter_items_list'     => __( 'Filter examples list', 'portfolio-plugin' ),
+			),
+			'supports' => array(
+				'title',
+				'revisions',
+				'page-attributes',
+			),
+			'taxonomies'            => array(),
+			'hierarchical'          => false,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 5,
+			'menu_icon'             => 'dashicons-media-code',
+			'show_in_admin_bar'     => true,
+			'show_in_nav_menus'     => true,
+			'can_export'            => true,
+			'has_archive'           => false,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
+			'rewrite'               => array(
+				'slug'       => 'examples',
+				'with_front' => false,
+				'pages'      => false,
+				'feeds'      => false,
+			),
+			'capability_type'       => 'post',
+		)
+	);
+
+	// Register Testimonials post type
+	register_post_type(
+		'pp_testimonials',
+		array(
+			'label'       => __( 'Testimonials', 'portfolio-plugin' ),
+			'description' => __( 'Recommendations and references.', 'portfolio-plugin' ),
+			'labels'      => array(
+				'name'                  => _x( 'Testimonials', 'Post Type General Name', 'portfolio-plugin' ),
+				'singular_name'         => _x( 'Testimonial', 'Post Type Singular Name', 'portfolio-plugin' ),
+				'menu_name'             => __( 'Testimonials', 'portfolio-plugin' ),
+				'name_admin_bar'        => __( 'Testimonial', 'portfolio-plugin' ),
+				'archives'              => __( 'Testimonial Archives', 'portfolio-plugin' ),
+				'parent_item_colon'     => __( 'Parent Testimonial:', 'portfolio-plugin' ),
+				'all_items'             => __( 'All Testimonials', 'portfolio-plugin' ),
+				'add_new_item'          => __( 'Add New Testimonial', 'portfolio-plugin' ),
+				'add_new'               => __( 'Add New', 'portfolio-plugin' ),
+				'new_item'              => __( 'New Testimonial', 'portfolio-plugin' ),
+				'edit_item'             => __( 'Edit Testimonial', 'portfolio-plugin' ),
+				'update_item'           => __( 'Update Testimonial', 'portfolio-plugin' ),
+				'view_item'             => __( 'View Testimonial', 'portfolio-plugin' ),
+				'search_items'          => __( 'Search Testimonial', 'portfolio-plugin' ),
+				'not_found'             => __( 'Not found', 'portfolio-plugin' ),
+				'not_found_in_trash'    => __( 'Not found in Trash', 'portfolio-plugin' ),
+				'featured_image'        => __( 'Featured Image', 'portfolio-plugin' ),
+				'set_featured_image'    => __( 'Set featured image', 'portfolio-plugin' ),
+				'remove_featured_image' => __( 'Remove featured image', 'portfolio-plugin' ),
+				'use_featured_image'    => __( 'Use as featured image', 'portfolio-plugin' ),
+				'insert_into_item'      => __( 'Insert into testimonial', 'portfolio-plugin' ),
+				'uploaded_to_this_item' => __( 'Uploaded to this testimonial', 'portfolio-plugin' ),
+				'items_list'            => __( 'Testimonial list', 'portfolio-plugin' ),
+				'items_list_navigation' => __( 'Testimonials list navigation', 'portfolio-plugin' ),
+				'filter_items_list'     => __( 'Filter testimonials list', 'portfolio-plugin' ),
+			),
+			'supports' => array(
+				'editor',
+				'title',
+				'revisions',
+				'page-attributes',
+			),
+			'taxonomies'            => array(),
+			'hierarchical'          => false,
+			'public'                => true,
+			'show_ui'               => true,
+			'show_in_menu'          => true,
+			'menu_position'         => 6,
+			'menu_icon'             => 'dashicons-heart',
+			'show_in_admin_bar'     => true,
+			'show_in_nav_menus'     => true,
+			'can_export'            => true,
+			'has_archive'           => false,
+			'exclude_from_search'   => false,
+			'publicly_queryable'    => true,
+			'rewrite'               => array(
+				'slug'       => 'testimonials',
+				'with_front' => false,
+				'pages'      => false,
+				'feeds'      => false,
+			),
+			'capability_type'       => 'post',
+		)
+	);
+}
+
+
+/**
+ * 2.0 Custom Post-type Redirect
  *
 **/
+add_action( 'template_redirect', 'portfolio_custom_post_type_template_redirect' );
+function portfolio_custom_post_type_template_redirect() {
 
-// Create function
-function pp_register_post_type( $singular, $plural, $text_domain, $description, $menu_position, $icon ) {
+	// Get the queried custom post type
+	$queried_post_type = get_query_var( 'post_type' );
 
-	// create labels array
-	$labels = array(
-		'name'                  => _x( $plural, 'Post Type General Name', 
-	                                   $text_domain ),
-	    'singular_name'         => _x( $singular, 'Post Type Singular Name',
-	                                   $text_domain ),
-	    'menu_name'             => __( $plural, 
-	                                   $text_domain ),
-	    'name_admin_bar'        => __( $singular, 
-	                                   $text_domain ),
-	    'archives'              => __( $singular . ' Archives', 
-	                                   $text_domain ),
-	    'parent_item_colon'     => __( 'Parent ' . $singular . ':', 
-	                                   $text_domain ),
-	    'all_items'             => __( 'All ' . $plural, 
-	                                   $text_domain ),
-	    'add_new_item'          => __( 'Add New ' . $singular, 
-	                                   $text_domain ),
-	    'add_new'               => __( 'Add New', 
-	                                   $text_domain ),
-	    'new_item'              => __( 'New ' . $singular, 
-	                                   $text_domain ),
-	    'edit_item'             => __( 'Edit ' . $singular, 
-	                                   $text_domain ),
-	    'update_item'           => __( 'Update ' . $singular, 
-	                                   $text_domain ),
-	    'view_item'             => __( 'View ' . $singular, 
-	                                   $text_domain ),
-	    'search_items'          => __( 'Search ' . $singular, 
-	                                   $text_domain ),
-	    'not_found'             => __( 'Not found',
-	                                   $text_domain ),
-	    'not_found_in_trash'    => __( 'Not found in Trash', 
-	                                   $text_domain ),
-	    'featured_image'        => __( 'Featured Image', 
-	                                   $text_domain ),
-	    'set_featured_image'    => __( 'Set featured image', 
-	                                   $text_domain ),
-	    'remove_featured_image' => __( 'Remove featured image', 
-	                                   $text_domain ),
-	    'use_featured_image'    => __( 'Use as featured image', 
-	                                   $text_domain ),
-	    'insert_into_item'      => __( 'Insert into ' . strtolower( $singular ), 
-	                                   $text_domain ),
-	    'uploaded_to_this_item' => __( 'Uploaded to this ' . strtolower( $singular ), 
-	                                   $text_domain ),
-	    'items_list'            => __( $singular . ' list', 
-	                                   $text_domain ),
-	    'items_list_navigation' => __( $plural . ' list navigation', 
-	                                   $text_domain ),
-	    'filter_items_list'     => __( 'Filter '. strtolower( $plural ) . ' list', 
-	                                   $text_domain ),
-	);
-
-	// Create rewrite array
-	$rewrite = array(
-	    'slug'                  => strtolower( $plural ),
-	    'with_front'            => false,
-	    'pages'                 => false,
-	    'feeds'                 => false,
-	);
-
-	// Create final args list
-	 $args = array(
-	    'label'                 => __( $plural, 
-	                                   $text_domain ),
-	    'description'           => __( $description,
-	                                   $text_domain ),
-	    'labels'                => $labels,
-	    'supports'              => array(
-	                                'title',
-	                                'editor',
-	                                'excerpt',
-	                                'thumbnail',
-	                                'revisions',
-	                                'page-attributes',
-	                             ),
-	    'taxonomies'            => array(),
-	    'hierarchical'          => false,
-	    'public'                => true,
-	    'show_ui'               => true,
-	    'show_in_menu'          => true,
-	    'menu_position'         => $menu_position,
-	    'menu_icon'             => $icon,
-	    'show_in_admin_bar'     => true,
-	    'show_in_nav_menus'     => true,
-	    'can_export'            => true,
-	    'has_archive'           => false,
-	    'exclude_from_search'   => false,
-	    'publicly_queryable'    => true,
-	    'rewrite'               => $rewrite,
-	    'capability_type'       => 'post',
-	);
-
-	// Create namespaced post type name
-	$post_type_name = 'pp_' . strtolower( $plural );
-
-	// Register the post type
-	register_post_type( $post_type_name, $args );
+	// Check if we're on a single and the queried post type is one of our custom post types
+	if ( is_single()
+	   && ( 'pp_examples' === $queried_post_type
+	   || 'pp_testimonials' === $queried_post_type ) ) {
+		wp_safe_redirect(
+			home_url(),
+			301
+		);
+		exit;
+	}
 }
-
-// Excecute function and create needed post types
-function pp_register_post_types() {
-	pp_register_post_type( 
-							'Example', 
-							'Examples', 
-							'portfolio-plugin', 
-							'Portfolio work pieces.',
-							5,
-							'dashicons-media-code' 
-						);
-	pp_register_post_type( 
-							'Testimonial',
-							'Testimonials',
-							'portfolio-plugin',	
-							'Recommendations and references.',
-							6,
-							'dashicons-heart' 
-						);
-}
-add_action( 'init', 'pp_register_post_types');
