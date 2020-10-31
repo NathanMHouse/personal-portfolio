d5d68a537fd74079bb5d8441e788a7493b1f6941
Nathan M. House <nathan.house@brainrider.com>
2020-10-31 11:34:25 -0400

    Removed arrow from title module

--- template-parts/module-title.php
+++ template-parts/module-title.php
@@ -14,17 +14,9 @@
 			// The title
 			?>
 			<div class="title-content-text col-md-6 col-md-offset-3">
-				<h2><?php the_title(); ?></h2>
+				<h1><?php the_title(); ?></h1>
 			</div><!-- .title-content-text -->
 
 		</div><!-- .title-content -->
 	</div><!-- .container -->
-
-	<?php
-
-	// The arrow
-	?>
-	<div class="title-section-arrow">
-	</div><!-- .title-section-arrow -->
-
 </div><!-- .title-section -->
