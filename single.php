ebde95d3ce0e70003e487f2e9c93924a96de7181
Nathan M. House <nathan.house@brainrider.com>
2021-01-29 11:54:57 -0500

    Remove TOC from notes (single)

--- single.php
+++ single.php
@@ -7,23 +7,12 @@
  * @package Portfolio
  */
 
-get_header(); ?>
+get_header(); 
+	while ( have_posts() ) :
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		the_post();
 
-		<?php
-		while ( have_posts() ) :
+		get_template_part( 'template-parts/content', get_post_format() );
 
-			the_post();
-
-			get_template_part( 'template-parts/content', get_post_format() );
-
-		endwhile; // End of the loop.
-		?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-<?php
+	endwhile; // End of the loop.
 get_footer();
