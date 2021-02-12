5401c67a143b7342af37890f6b6b90c8c96a34b9
Nathan M. House <nathan.house@brainrider.com>
2021-02-12 10:30:57 -0500

    Update post excerpt

---
+++ template-parts/content-excerpt.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Template part for displaying post excerpts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Portfolio
+ */
+?>
+
+<li class="posts__post"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
