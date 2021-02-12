4a16463c3043f17215589a7e304c1cae374ec365
Nathan M. House <nathan.house@brainrider.com>
2021-02-12 10:31:23 -0500

    Update title module
    * Updated to allow for posts page title.

--- template-parts/module-title.php
+++ template-parts/module-title.php
@@ -9,14 +9,20 @@
 <div class="title-section">
 	<div class="container">
 		<div class="title-content row">
-
 			<?php
 			// The title
 			?>
 			<div class="title-content-text col-md-8 col-md-offset-2">
-				<h1><?php the_title(); ?></h1>
+				<h1>
+					<?php
+					if ( is_home() && ! is_front_page() ) :
+						_e( 'Notes', 'portfolio' );
+					else :
+						the_title();
+					endif;
+					?>
+				</h1>
 			</div><!-- .title-content-text -->
-
 		</div><!-- .title-content -->
 	</div><!-- .container -->
 </div><!-- .title-section -->
